043175334530ab61ee60873fccc2522e47f21b01
Daniel Martí <mvdan@mvdan.cc>
2013-06-25 17:46:41 +0200

    Save uci log to tmp

--- wibed-system/files/etc/uci-defaults/try_prepare_usb.sh
+++ wibed-system/files/etc/uci-defaults/try_prepare_usb.sh
@@ -3,7 +3,7 @@
 for device in /dev/sda /dev/sdb; do
     if [ -b $device ]; then
         blkid | grep ${device}1.*wibed-overlay && continue
-        wibed.prepare-usb $device &>/wibed.prepare-usb.log && reboot
+        wibed.prepare-usb $device &>/tmp/wibed.prepare-usb.log && reboot
         break
     fi
 done
