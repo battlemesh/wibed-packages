d688e4b1f06731b814d2b414c98408d439f1d839
Gui Iribarren <gui@altermundi.net>
2013-10-17 05:28:27 +0200

    remove unnecessary reboot after partitioning usb device

--- wibed-system/files/etc/uci-defaults/try_prepare_usb.sh
+++ wibed-system/files/etc/uci-defaults/try_prepare_usb.sh
@@ -3,7 +3,7 @@
 for device in /dev/sd*; do
     if [ -b $device ]; then
         blkid -t LABEL=wibed-overlay ${device}1 &>/dev/null && continue
-        wibed-prepare-usb $device &>/root/wibed-prepare-usb.log && reboot
+        wibed-prepare-usb $device &>/root/wibed-prepare-usb.log
         break
     fi
 done
