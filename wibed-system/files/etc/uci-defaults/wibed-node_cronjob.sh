1c43a066d22c55e87dc2ca6c4d836137989469e0
Gui Iribarren <gui@altermundi.net>
2013-10-26 02:35:18 +0200

    add cronjob (on first boot) that runs wibed-node every 15 seconds

---
+++ wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if ! ( grep -q "/usr/sbin/wibed-node" /etc/crontabs/root 2>/dev/null ) ; then
+	echo "*/1 * * * * /usr/sbin/wibed-node" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 30 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 45 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
+	/etc/init.d/cron enable
+	/etc/init.d/cron restart
+fi
+
