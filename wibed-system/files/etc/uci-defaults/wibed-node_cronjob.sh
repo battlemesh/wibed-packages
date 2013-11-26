995c4a4f40671e240fa948adcd3738c7f9ddbf9a
Manos Dimogerontakis <emm.dimogerontakis@gmail.com>
2013-11-26 18:03:12 +0100

    Log the last run of /usr/sbin/wibed-node in /root/wibed-node.log and the last run of /usr/sbin/wibed-status to /root/wibed-status.log

--- wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
+++ wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
@@ -2,10 +2,10 @@
 
 if ! ( grep -q "/usr/sbin/wibed-node" /etc/crontabs/root 2>/dev/null ) ; then
 	echo "*/1 * * * * /usr/sbin/wibed-node" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 30 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 45 ; /usr/sbin/wibed-node" >> /etc/crontabs/root
-	echo "*/1 * * * * /usr/sbin/wibed-status" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node > /root/wibed-node.log" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 30 ; /usr/sbin/wibed-node > /root/wibed-node.log" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 45 ; /usr/sbin/wibed-node > /root/wibed-node.log" >> /etc/crontabs/root
+	echo "*/1 * * * * /usr/sbin/wibed-status > /root/wibed-status.log" >> /etc/crontabs/root
 	/etc/init.d/cron enable
 	/etc/init.d/cron restart
 fi
