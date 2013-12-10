984432c74eeac564f68bd3f43b7bc10cc2363090
Pau Escrich <p4u@dabax.net>
2013-12-10 19:42:54 +0100

    Small fixes

--- wibed-system/files/etc/uci-defaults/try_prepare_usb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for device in /dev/sd*; do
-    if [ -b $device ]; then
-        blkid -t LABEL=wibed-overlay ${device}1 &>/dev/null && continue
-        wibed-prepare-usb $device &>/root/wibed-prepare-usb.log
-        break
-    fi
-done
--- wibed-system/files/etc/uci-defaults/wibed-config.sh
+++ wibed-system/files/etc/uci-defaults/wibed-config.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+for device in /dev/sd*; do
+    if [ -b $device ]; then
+        #blkid -t LABEL=wibed-overlay ${device}1 >/dev/null && continue
+        wibed-prepare-usb $device >/root/wibed-prepare-usb.log
+        break
+    fi
+done
+
 wibed-config 2>&1 > /root/wibed-config.log
+
+(sleep 10 && reboot) &
