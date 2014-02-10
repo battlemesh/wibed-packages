b901f76ed56e1fc9d83e7f62d4db71b7d7f079aa
manos <savemanos@gmail.com>
2014-02-10 16:18:00 +0100

    Fix bug on logging node state

--- wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
+++ wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if ! ( grep -q "/usr/sbin/wibed-node" /etc/crontabs/root 2>/dev/null ) ; then
-	echo "*/1 * * * * /usr/sbin/wibed-node" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node > /tmp/wibed-node.log" >> /etc/crontabs/root
+	echo "*/1 * * * * /usr/sbin/wibed-node > /tmp/wibed-node.log" >> /etc/crontabs/root
+	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
 	echo "*/1 * * * * sleep 30 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
 	echo "*/1 * * * * sleep 45 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
 	echo "*/1 * * * * /usr/sbin/wibed-status > /root/wibed-status.log" >> /etc/crontabs/root
