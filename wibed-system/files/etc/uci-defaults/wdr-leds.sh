6998a87528d670b62c2f38e24ccc7954edc52ca4
Eloi Carbo <eloicaso@gmail.com>
2014-02-21 17:11:20 +0100

    Modify wibed configuration regarding LEDs of different router models naming.

--- wibed-system/files/etc/uci-defaults/wdr-leds.sh
+++ wibed-system/files/etc/uci-defaults/wdr-leds.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo 'echo timer > /sys/class/leds/tp-link\:blue\:qss/trigger' >> /etc/rc.local
+#Checking which leds have your router
+
+#DEFAULT LEDS WDR4300
+DEFAULT_SERVER_LED=/sys/class/leds/tp-link\:blue\:qss
+
+#ADD HERE YOUR NODE LEDS {WDR4300, WDR4900}
+QSS=/sys/class/leds/tp-link\:blue\:qss
+WPS=/sys/class/leds/tp-link\:blue\:wps
+
+ls ${WPS} > /dev/null 2>&1
+if [ $? == 0 ]; then
+	DEFAULT_SERVER_LED=$WPS
+fi
+
+echo 'echo timer > ${DEFAULT_SERVER_LED}/trigger' >> /etc/rc.local
