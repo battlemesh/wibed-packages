429e7b33eccbe3b81558c7200051ac09acec713c
Pau Escrich <p4u@dabax.net>
2014-11-12 11:53:28 +0100

    Add random time offset for server API calls Reduce the recovery time from 60 to 30 minutes

--- wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
+++ wibed-system/files/etc/uci-defaults/wibed-node_cronjob.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 if ! ( grep -q "/usr/sbin/wibed-node" /etc/crontabs/root 2>/dev/null ) ; then
-	echo "*/1 * * * * /usr/sbin/wibed-node > /tmp/wibed-node.log" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 15 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 30 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
-	echo "*/1 * * * * sleep 45 ; /usr/sbin/wibed-node >> /tmp/wibed-node.log" >> /etc/crontabs/root
-	echo "*/1 * * * * /usr/sbin/wibed-status > /root/wibed-status.log" >> /etc/crontabs/root
+	SEED="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
+	TIME="$(( $SEED % 15 ))"
+	echo "* * * * * (sleep $((0+$TIME)) ; /usr/sbin/wibed-node >> /tmp/wibed-node.log)" >> /etc/crontabs/root
+	echo "* * * * * (sleep $((15+$TIME)) ; /usr/sbin/wibed-node >> /tmp/wibed-node.log)" >> /etc/crontabs/root
+	echo "* * * * * (sleep $((30+$TIME)) ; /usr/sbin/wibed-node >> /tmp/wibed-node.log)" >> /etc/crontabs/root
+	echo "* * * * * (sleep $((45+$TIME)) ; /usr/sbin/wibed-node >> /tmp/wibed-node.log)" >> /etc/crontabs/root
+	echo "* * * * * (sleep $((5+$TIME)) /usr/sbin/wibed-status > /root/wibed-status.log)" >> /etc/crontabs/root
 	/etc/init.d/cron enable
 	/etc/init.d/cron restart
 fi
-
